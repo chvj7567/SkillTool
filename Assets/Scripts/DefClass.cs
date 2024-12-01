2dfdb9b9047f41b830c8db63b525d83edc77d7a1
chvj7567 <chvj7567@naver.com>
2024-12-01 13:11:30 +0900

    [Simple] 변수 타입 수정

--- Assets/Scripts/DefClass.cs
+++ Assets/Scripts/DefClass.cs
@@ -71,7 +71,7 @@ public partial class DefClass
     [Serializable]
     public class TargetInfo
     {
-        public GameObject target = null;
+        public CHUnitData target = null;
         public float distance = -1f;
     }
 
